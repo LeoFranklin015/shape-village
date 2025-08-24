6294c3677a528b239c512e4ed8ae180580569982
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2025-08-24 17:36:52 +0530

    feat: add navigation link to Villages in Navbar component

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CustomConnectButton } from "./ConnectButton";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -7,14 +8,20 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Left side - Village title */}
-          <div className="flex items-center">
+          <div className="flex items-center space-x-6">
             <h1 className="game-text text-xl font-bold text-white">
               ShapeVillage
             </h1>
           </div>
 
           {/* Right side - Create Village button */}
-          <div className="flex items-center">
+          <div className="flex items-center space-x-4">
+            <Link
+              href="/village"
+              className="game-text text-sm text-white/80 hover:text-white transition-colors duration-200 font-medium"
+            >
+              Villages
+            </Link>
             <CustomConnectButton />
           </div>
         </div>
